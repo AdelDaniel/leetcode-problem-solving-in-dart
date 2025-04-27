8333c278b96936e5db2c91ed6049a68e2fd34ded
adel-daniel <adel.daniel.222@gmail.com>
2025-04-27 23:16:04 +0300

    Enhance findMin function with additional comments for clarity; add tests for edge cases with duplicate values

--- leetcode/153.find_minimum_in_rotated_sorted_array.dart
+++ leetcode/153.find_minimum_in_rotated_sorted_array.dart
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+import 'dart:math';
+
 import 'package:test/test.dart';
 
 void main() {
@@ -7,27 +9,75 @@ void main() {
 }
 
 class Solution {
+  //! Will Fail at [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3]]
   int findMin(List<int> nums) {
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    if (nums.length == 2) {
+      return min(nums[0], nums[1]);
+    }
+    if (nums[0] < nums[nums.length - 1]) {
+      return nums[0];
+    }
+
     int left = 0;
     int right = nums.length - 1;
-    int middle = left + (right - left) ~/ 2;
+    int mid = left + (right - left) ~/ 2;
 
-    do {
-      /// if the most right side is less than the middle
-      /// then the minimum is on the right side
-      /// else
-      /// - the right is sorted
-      /// - the minimum number is on the left side
-      if (nums[middle] > nums[right]) {
-        left = middle + 1;
-      } else {
-        right = middle;
+    while (left <= right) {
+      // Equivalent to (left + right) / 2 in integer division
+      mid = left + (right - left) ~/ 2;
+
+      if (mid < right && nums[mid] > nums[mid + 1]) {
+        return nums[mid + 1];
+      }
+      if (mid > left && nums[mid - 1] > nums[mid]) {
+        return nums[mid];
       }
 
-      middle = left + (right - left) ~/ 2;
-    } while (left < right);
-    return nums[middle];
+      if (nums[left] < nums[mid]) {
+        // Discard the sorted part (increasing part)
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return 0;
   }
+
+  //! This Solution Will fail in this case [3,3,3,1,3]
+  // int findMin(List<int> nums) {
+  //   int left = 0;
+  //   int right = nums.length - 1;
+  //   int middle = left + (right - left) ~/ 2;
+
+  //   do {
+  //     /// Both sides are sorted -> min in left
+  //     /// right side is sorted && left side not sorted -> min in left
+  //     /// left side is sorted && right side not sorted -> min in rihgt
+  //     ///
+  //     ///
+  //     /// Check:((is the rigth side not sorted?))
+  //     /// If you check the right side sorted then you don't know if the left side is sorted too or not.
+  //     ///
+  //     /// if the most right side is less than the middle
+  //     /// then: the right side is not sorted
+  //     /// then: the minimum is on the right side
+  //     /// else
+  //     /// - the right is sorted
+  //     /// - the minimum number is on the left side
+  //     if (nums[middle] > nums[right]) {
+  //       left = middle + 1;
+  //     } else {
+  //       right = middle;
+  //     }
+
+  //     middle = left + (right - left) ~/ 2;
+  //   } while (left < right);
+  //   return nums[middle];
+  // }
 }
 
 void runTests() {
@@ -136,6 +186,25 @@ void runTests() {
     // Special Edge Cases (note: problem specifies unique elements)
     test('Nearly sorted with small rotation', () {
       expect(s.findMin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]), equals(0));
+    });
+
+    test('Nearly sorted with small rotation: [3,3,1,3]', () {
+      expect(s.findMin([3, 3, 1, 3]), equals(1));
+    });
+
+    test('Nearly sorted with small rotation: [3,3,3,1,3] ', () {
+      expect(s.findMin([3, 3, 3, 1, 3]), equals(1));
+    });
+
+    test('Nearly sorted with small rotation: [3, 3, 1, 2, 3, 3, 3]', () {
+      expect(s.findMin([3, 3, 1, 2, 3, 3, 3]), equals(1));
+    });
+
+    test(
+        'Nearly sorted with small rotation: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3]',
+        () {
+      expect(s.findMin([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3]),
+          equals(1));
     });
   });
 }
