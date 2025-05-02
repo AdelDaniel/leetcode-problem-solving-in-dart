6083db74c3d239e28804c4aec222eb3890450fec
adel-daniel <adel.daniel.222@gmail.com>
2025-05-03 02:08:24 +0300

    Add Lists class with rotate and rotateByReversing methods; implement array reversal logic

--- leetcode/189.rotate_array.dart
+++ leetcode/189.rotate_array.dart
@@ -12,15 +12,57 @@ void main() {
 }
 
 class Solution {
-  ////! My First Solution
+  ////! Best Solution
+  /// After implement this solution will give you the same shape when you rotate the array
+  /// TC: O(3n) == O(n) --- SC: O(1)
   void rotate(List<int> nums, int k) {
-    if (nums.length <= 1 || k == 0) return;
-    k = k % nums.length;
-    List<int> range = nums.getRange(0, nums.length - k).toList();
-    nums
-      ..removeRange(0, nums.length - k)
-      ..addAll(range);
+    if (nums.isEmpty || k == 0) return;
+
+    final n = nums.length;
+    k %= n;
+
+    /// Reverse All The Array [1,2,3,4] --> [4,3,2,1]
+    reverse(nums, 0, n - 1);
+
+    /// Reverse First K Elements
+    /// K = 2 --> from 0-2 --> [4,3,2,1] --> [3,4,2,1]
+    reverse(nums, 0, k - 1);
+
+    /// Reverse Remaining Elements
+    /// K = 2 --> from 2-3 -->[3,4,2,1] --> [3,4,1,2]
+    reverse(nums, k, n - 1);
   }
+
+  /// How to reverse an array
+  void reverse(List<int> nums, int start, int end) {
+    while (start < end) {
+      final temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+  }
+
+  ////! My First Solution
+  // void rotate(List<int> nums, int k) {
+  //   if (nums.length <= 1 || k == 0) return;
+  //   k = k % nums.length;
+  //   List<int> range = nums.getRange(0, nums.length - k).toList();
+  //   nums
+  //     ..removeRange(0, nums.length - k)
+  //     ..addAll(range);
+  // }
+
+  ////! Time Limit --> Slow
+  // void rotate(List<int> nums, int k) {
+  //   if (nums.length <= 1 || k == 0) return;
+  //   k = k % nums.length;
+
+  //   for (int i = 0; i < k; i++) {
+  //     nums.insert(0, nums.removeLast());
+  //   }
+  // }
 }
 
 void runTests() {
@@ -28,6 +70,13 @@ void runTests() {
 
   group('Rotate Array', () {
     // Basic examples from problem statement
+
+    test('Example 1: [1,2,3,4], k=2', () {
+      final nums = [1, 2, 3, 4];
+      s.rotate(nums, 2);
+      expect(nums, equals([3, 4, 1, 2]));
+    });
+
     test('Example 1: [1,2,3,4,5,6,7], k=3', () {
       final nums = [1, 2, 3, 4, 5, 6, 7];
       s.rotate(nums, 3);
